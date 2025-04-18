f6fe7983f1b9e7c04032ce16d3731d45a823e9f8
FrancoGarbarino <115745382+FrancoGarbarino@users.noreply.github.com>
2025-04-17 23:56:57 -0300

    Update PruebasUnitarias.pl

--- PruebasUnitarias.pl
+++ PruebasUnitarias.pl
@@ -15,7 +15,30 @@
     % Prueba de color
     test(color_rose) :- color(rose, rojo).
     test(color_lily) :- color(lily, blanco).
+    
+    % Punto 2 
 
+    % Prueba de riego_especial
+    test(riego_especial_rose, fail) :- riego_especial(rose).
+    test(riego_especial_lily, fail) :- riego_especial(lily).
+
+    % Prueba de atraen_insectos
+    test(atraen_insectos_rose, nondet) :- atraen_insectos(rose).
+    test(atraen_insectos_lily, fail) :- atraen_insectos(lily).
+
+    test(considerada_alta_sunflower, fail) :- considerada_alta(sunflower).
+    test(considerada_alta_ivy) :- considerada_alta(ivy).
+    test(considerada_alta_cactus) :- considerada_alta(cactus).
+    test(considerada_alta_tulip, fail) :- considerada_alta(tulip).
+
+    % Punto 3 
+
+    % Prueba de planta_corta_flor
+    test(planta_corta_flor_lily) :- planta_corta_flor(lily).
+    test(planta_corta_flor_orchid) :- planta_corta_flor(orchid).
+    test(planta_corta_flor_tulip) :- planta_corta_flor(tulip).
+    test(planta_corta_flor_sunflower, fail) :- planta_corta_flor(sunflower).
+    
     % Punto 4
     
     % Prueba de coincide_con_observador
