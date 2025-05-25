e48783f80b87363b56a629ce769058763d3c21b8
FrancoGarbarino <115745382+FrancoGarbarino@users.noreply.github.com>
2025-05-25 20:40:28 -0300

    Update PruebasUnitarias.pl

--- PruebasUnitarias.pl
+++ PruebasUnitarias.pl
@@ -50,34 +50,16 @@
     test(planta_corta_flor_orchid) :- planta_corta_flor(orchid).
     test(planta_corta_flor_tulip) :- planta_corta_flor(tulip).
     test(planta_corta_flor_sunflower, fail) :- planta_corta_flor(sunflower).
+   % Punto 4 - cumple_todas_las_pistas/2
+   
+    test(cumple_pistas_lily_flor_blanca) :- cumple_todas_las_pistas(lily, flor_blanca).
+    test(cumple_pistas_cactus_planta_verde) :- cumple_todas_las_pistas(cactus, planta_verde).
+    test(cumple_pistas_ivy_enredadera) :- cumple_todas_las_pistas(ivy, enredadera_verde).
+    test(cumple_pistas_fern_flor_blanca, fail) :- cumple_todas_las_pistas(fern, flor_blanca).
     
-    % Punto 4
+    % Punto 5 - atrae_mas_visitas/1
     
-    % Prueba de coincide_con_observador
-    test(coincide_con_observador_positiva,nondet) :-
-        coincide_con_observador(rose, arbol_rojo).
-    
-    test(coincide_con_observador_negativa, fail) :-
-        coincide_con_observador(lily, arbol_rojo).
+    test(atrae_mas_visitas_rose, nondet) :- atrae_mas_visitas(rose).
+    test(atrae_mas_visitas_lily, fail) :- \+ atrae_mas_visitas(lily).
 
-    test(coincide_con_observador_positiva_2,nondet) :-
-        coincide_con_observador(orchid, flor_morada).
-
-    test(coincide_con_observador_negativa_2, fail) :-
-        coincide_con_observador(fern, flor_morada).
-
-    % Punto 5
-
-    test(atrae_mas_visitas_positiva,nondet) :-
-        atrae_mas_visitas(rose).
-
-    test(atrae_mas_visitas_negativa,nondet) :-
-        atrae_mas_visitas(sunflower).
-
-    test(atrae_mas_visitas_negativa_2,nondet) :-
-        atrae_mas_visitas(fern).
-
-    test(atrae_mas_visitas_positiva_2,nondet) :-
-        atrae_mas_visitas(orchid).
-        
 :- end_tests(pruebaPlantas).
